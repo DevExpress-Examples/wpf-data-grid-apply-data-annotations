cac34efbef19dab0545b0cd9ef85f9f238255b02
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Apply Data Annotations/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Apply_Data_Annotations {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            grid.DataSource = ProductList.GetData();
+        }
+    }
+}
