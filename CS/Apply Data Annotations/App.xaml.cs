1075929f4388560599ae636f1e602087336495ec
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Apply Data Annotations/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Apply_Data_Annotations {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
---
+++ VB/Apply Data Annotations/MainWindow.xaml.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace Apply_Data_Annotations
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = ProductList.GetData()
+		End Sub
+	End Class
+End Namespace
