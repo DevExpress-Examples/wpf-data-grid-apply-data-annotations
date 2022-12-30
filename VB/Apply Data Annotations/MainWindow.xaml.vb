d317032a27432be06ab63f0e7b5514d3433ca492
DevExpressExampleBot <fakeEmail>
2022-12-30 11:47:58 +0400

    Source auto update [skip ci]

--- VB/Apply Data Annotations/MainWindow.xaml.vb
+++ VB/Apply Data Annotations/MainWindow.xaml.vb
@@ -1,15 +1,16 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 
 Namespace Apply_Data_Annotations
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = ProductList.GetData()
-		End Sub
-	End Class
+
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = ProductList.GetData()
+        End Sub
+    End Class
 End Namespace
--- VB/Apply Data Annotations/Products.vb
+++ VB/Apply Data Annotations/Products.vb
@@ -1,80 +1,39 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.ComponentModel.DataAnnotations
 
 Public Class Product
-	Private privateCompanyName As String
-	<Display(Order := 0, ShortName := "Company")> _
-	Public Property CompanyName() As String
-		Get
-			Return privateCompanyName
-		End Get
-		Set(ByVal value As String)
-			privateCompanyName = value
-		End Set
-	End Property
-	Private privateCountry As String
-	Public Property Country() As String
-		Get
-			Return privateCountry
-		End Get
-		Set(ByVal value As String)
-			privateCountry = value
-		End Set
-	End Property
-	Private privateCity As String
-	Public Property City() As String
-		Get
-			Return privateCity
-		End Get
-		Set(ByVal value As String)
-			privateCity = value
-		End Set
-	End Property
-	Private privateUnitPrice As Double
-	Public Property UnitPrice() As Double
-		Get
-			Return privateUnitPrice
-		End Get
-		Set(ByVal value As Double)
-			privateUnitPrice = value
-		End Set
-	End Property
-	Private privateQuantity As Integer
-	Public Property Quantity() As Integer
-		Get
-			Return privateQuantity
-		End Get
-		Set(ByVal value As Integer)
-			privateQuantity = value
-		End Set
-	End Property
-	Private privateAdditionalInfo As String
-	<Display(AutoGenerateField := False, Description := "This field is hidden")> _
-	Public Property AdditionalInfo() As String
-		Get
-			Return privateAdditionalInfo
-		End Get
-		Set(ByVal value As String)
-			privateAdditionalInfo = value
-		End Set
-	End Property
+
+    <Display(Order:=0, ShortName:="Company")>
+    Public Property CompanyName As String
+
+    Public Property Country As String
+
+    Public Property City As String
+
+    <[ReadOnly](True)>
+    Public Property UnitPrice As Double
+
+    <Display(Description:="This field is hidden", Order:=-1)>
+    Public Property Quantity As Integer
+
+    <Display(AutoGenerateField:=False, Description:="This column isn't created")>
+    Public Property AdditionalInfo As String
 End Class
 
 Public Class ProductList
-	Public Shared Function GetData() As List(Of Product)
-		Dim list As New List(Of Product)()
-		list.Add(New Product() With {.CompanyName = "Island Trading", .Country = "UK", .City = "Cowes", .UnitPrice = 19, .Quantity = 10})
-		list.Add(New Product() With {.CompanyName = "Reggiani Caseifici", .Country = "Italy", .City = "Reggio Emilia", .UnitPrice = 12.5, .Quantity = 16})
-		list.Add(New Product() With {.CompanyName = "Ricardo Adocicados", .Country = "Brazil", .City = "Rio de Janeiro", .UnitPrice = 19, .Quantity = 12})
-		list.Add(New Product() With {.CompanyName = "QUICK-Stop", .Country = "Germany", .City = "QUICK-Stop", .UnitPrice = 22, .Quantity = 50})
-		list.Add(New Product() With {.CompanyName = "Split Rail Beer & Ale", .Country = "USA", .City = "Reggio Emilia", .UnitPrice = 18, .Quantity = 20})
-		list.Add(New Product() With {.CompanyName = "Ernst Handel", .Country = "Austria", .City = "Graz", .UnitPrice = 21, .Quantity = 52})
-		list.Add(New Product() With {.CompanyName = "Save-a-lot Markets", .Country = "USA", .City = "Boise", .UnitPrice = 7.75, .Quantity = 120})
-		list.Add(New Product() With {.CompanyName = "Tortuga Restaurante", .Country = "Mexico", .City = "México D.F.", .UnitPrice = 21, .Quantity = 15})
-		list.Add(New Product() With {.CompanyName = "Bottom-Dollar Markets", .Country = "Canada", .City = "Tsawwassen", .UnitPrice = 44, .Quantity = 16})
-		Return list
-	End Function
+
+    Shared Public Function GetData() As List(Of Product)
+        Dim list As List(Of Product) = New List(Of Product)()
+        list.Add(New Product() With {.CompanyName = "Island Trading", .Country = "UK", .City = "Cowes", .UnitPrice = 19, .Quantity = 10})
+        list.Add(New Product() With {.CompanyName = "Reggiani Caseifici", .Country = "Italy", .City = "Reggio Emilia", .UnitPrice = 12.5, .Quantity = 16})
+        list.Add(New Product() With {.CompanyName = "Ricardo Adocicados", .Country = "Brazil", .City = "Rio de Janeiro", .UnitPrice = 19, .Quantity = 12})
+        list.Add(New Product() With {.CompanyName = "QUICK-Stop", .Country = "Germany", .City = "QUICK-Stop", .UnitPrice = 22, .Quantity = 50})
+        list.Add(New Product() With {.CompanyName = "Split Rail Beer & Ale", .Country = "USA", .City = "Reggio Emilia", .UnitPrice = 18, .Quantity = 20})
+        list.Add(New Product() With {.CompanyName = "Ernst Handel", .Country = "Austria", .City = "Graz", .UnitPrice = 21, .Quantity = 52})
+        list.Add(New Product() With {.CompanyName = "Save-a-lot Markets", .Country = "USA", .City = "Boise", .UnitPrice = 7.75, .Quantity = 120})
+        list.Add(New Product() With {.CompanyName = "Tortuga Restaurante", .Country = "Mexico", .City = "México D.F.", .UnitPrice = 21, .Quantity = 15})
+        list.Add(New Product() With {.CompanyName = "Bottom-Dollar Markets", .Country = "Canada", .City = "Tsawwassen", .UnitPrice = 44, .Quantity = 16})
+        Return list
+    End Function
 End Class
